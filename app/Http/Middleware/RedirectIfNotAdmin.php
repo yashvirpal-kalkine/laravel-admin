4c0bac7fa8257620f18e7fbd7313fc8fb44a2087
yashirpal <yashveersoftwere06@gmail.com>
2025-10-12 14:30:46 +0530

    Dashboard and Login Middleware

---
+++ app/Http/Middleware/RedirectIfNotAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectIfNotAdmin
+{
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // If not logged in as admin
+        if (!Auth::guard('admin')->check()) {
+            return redirect()->route('admin.login');
+        }
+
+        // If already logged in, continue to dashboard or requested page
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,38 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
 use App\Http\Controllers\Admin\Auth\LoginController;
 
 Route::prefix('admin')->name('admin.')->group(function () {
-    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-    Route::post('/login', [LoginController::class, 'login']);
-    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-    Route::middleware('auth:admin')->group(function () {
+    Route::middleware('admin.guest')->group(function () {
+        Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+        Route::post('/login', [LoginController::class, 'login']);
+    });
+
+    Route::middleware('admin')->group(function () {
         Route::get('/dashboard', function () {
             return view('admin.dashboard');
         })->name('dashboard');
+
+        Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
     });
 });
+
+
+// use App\Http\Controllers\Admin\Auth\LoginController;
+
+// Route::prefix('admin')->name('admin.')->group(function () {
+//     Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+//     Route::post('/login', [LoginController::class, 'login']);
+//     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+//     Route::middleware('auth:admin')->group(function () {
+//         Route::get('/dashboard', function () {
+//             return view('admin.dashboard');
+//         })->name('dashboard');
+//     });
+// });
 
 
 
