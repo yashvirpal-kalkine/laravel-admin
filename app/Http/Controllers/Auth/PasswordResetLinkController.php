2b9789fbfd2e588c94e397b7abf574f0dae64cd8
Yashvir Pal <yashvir.pal@kalkine.co.in>
2026-01-03 14:01:06 +0530

    Reset password and send link

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\View\View;
 
+use Illuminate\Validation\ValidationException;
+
 class PasswordResetLinkController extends Controller
 {
     /**
@@ -23,22 +25,78 @@ class PasswordResetLinkController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): RedirectResponse
+
+
+    public function store(Request $request)
     {
-        $request->validate([
-            'email' => ['required', 'email'],
-        ]);
+        try {
+            $request->validate([
+                'email' => ['required', 'email'],
+            ]);
 
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
+            $status = Password::sendResetLink(
+                $request->only('email')
+            );
 
-        return $status == Password::RESET_LINK_SENT
-                    ? back()->with('status', __($status))
-                    : back()->withInput($request->only('email'))
-                        ->withErrors(['email' => __($status)]);
+            if ($status === Password::RESET_LINK_SENT) {
+
+                //  AJAX response
+                if ($request->ajax()) {
+                    return response()->json([
+                        'success' => true,
+                        'message' => 'Password reset link has been sent to your email address.',
+                    ], 200);
+                }
+
+                //  Normal redirect
+                return back()->with('status', __($status));
+            }
+
+            //  Failed case
+            throw ValidationException::withMessages([
+                'email' => [__($status)],
+            ]);
+
+        } catch (ValidationException $e) {
+
+            if ($request->ajax()) {
+                return response()->json([
+                    'success' => false,
+                    'errors' => $e->errors(),
+                ], 422);
+            }
+
+            throw $e;
+
+        } catch (\Exception $e) {
+
+            if ($request->ajax()) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Something went wrong. Please try again later.',
+                ], 500);
+            }
+
+            return back()->with('error', 'Something went wrong.');
+        }
     }
+
+    // public function store(Request $request): RedirectResponse
+    // {
+    //     $request->validate([
+    //         'email' => ['required', 'email'],
+    //     ]);
+
+    //     // We will send the password reset link to this user. Once we have attempted
+    //     // to send the link, we will examine the response then see the message we
+    //     // need to show to the user. Finally, we'll send out a proper response.
+    //     $status = Password::sendResetLink(
+    //         $request->only('email')
+    //     );
+
+    //     return $status == Password::RESET_LINK_SENT
+    //                 ? back()->with('status', __($status))
+    //                 : back()->withInput($request->only('email'))
+    //                     ->withErrors(['email' => __($status)]);
+    // }
 }
